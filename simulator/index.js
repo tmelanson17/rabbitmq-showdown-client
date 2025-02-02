74be9404fe79ce7e44faec368f5bc36a4d44a698
tmelanson17 <tmelanson17@gmail.com>
2025-02-02 11:11:46 -0800

    Working simulator with two players

--- simulator/index.js
+++ simulator/index.js
@@ -5,7 +5,28 @@ var amqp = require('amqplib/callback_api');
 
 stream = new Sim.BattleStream();
 
-(async () => {
+const PlayerNames = {
+    p1: 'Alice',
+    p2: 'Bob'
+};
+const QueuesFromPlayer = {
+    p1: 'p1',
+    p2: 'p2'
+};
+const QueuesToPlayer = {
+    p1: 'from_p1',
+    p2: 'from_p2'
+};
+
+publishMessage = function (channel, queue, message) {
+    channel.assertQueue(queue, {
+        durable: false
+    });
+    channel.sendToQueue(queue, Buffer.from(message));
+    console.log(" [x] Sent %s", message);
+}
+
+streamReader = async (channel) => {
     for await (const output of stream) {
         // Split output by line.
         const lines = output.split('\n');
@@ -17,26 +38,37 @@ stream = new Sim.BattleStream();
             console.log(`${side} has updated.`);
 
             // Get player.
-            const player = side === 'p1' ? 'Alice' : 'Bob';
+            const player = PlayerNames[side];
 
             // Get hidden state.
             const state = lines[2].split('|')[2];
 
             // Log player and health.
             console.log(`${player} state: ${state}.`);
+
+            // Check if state contains "[Invalid choice]".
+            if (state.includes('[Invalid choice]')) {
+                console.log(`${player} has made an invalid choice.`);
+                publishMessage(channel, QueuesToPlayer[side], 'invalid');
+            } else {
+                // Publish to player queue.
+                publishMessage(channel, QueuesToPlayer[side], state);
+            }
+
         }
 
         // If the first line is update, print to both players.
         if (lines[0].startsWith('update')) {
             // Log all lines except the first one.
             lines.slice(1).forEach(line => console.log(line));
+            var state = lines.slice(1).join('\n');
+            // TODO: Update this to use global player names.
+            publishMessage(channel, QueuesToPlayer.p1, state);
+            publishMessage(channel, QueuesToPlayer.p2, state);
         }
     }
-})();
+};
 
-stream.write(`>start {"formatid":"gen7randombattle"}`);
-stream.write(`>player p1 {"name":"Alice"}`);
-stream.write(`>player p2 {"name":"Bob"}`);
 
 writer = function (channel, queue) {
     channel.assertQueue(queue, {
@@ -68,5 +100,20 @@ connector = function (queue) {
     });
 }
 
-connector('p1');
-connector('p2');
+stream.write(`>start {"formatid":"gen8randombattle"}`);
+Object.entries(PlayerNames).forEach(([side, player]) => {
+    stream.write(`>player ${side} {"name":"${player}"}`);
+    connector(QueuesFromPlayer[side]);
+});
+amqp.connect('amqp://localhost', function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(async function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        streamReader(channel);
+    });
+});
